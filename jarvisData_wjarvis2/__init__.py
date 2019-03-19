d77803eb656ca060fa6a56748c66ad6b0e372ebf
wjarvis2 <wjarvis2@users.noreply.github.com>
2019-03-18 21:14:47 -0400

    The good goods are getting better

---
+++ jarvisData_wjarvis2/__init__.py
@@ -0,0 +1,11 @@
+"""
+jarvisData - The goods you never knew you needed.
+"""
+
+import pandas as pd
+import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.ones(50)) 
+
+
---
+++ jarvisData_wjarvis2/df_utils.py
@@ -0,0 +1,10 @@
+"""
+Utility functions for working with dataframes
+"""
+
+import pandas as pd
+
+
+TEST_df = pd.DataFrame([1, 2, 3]) 
+
+
